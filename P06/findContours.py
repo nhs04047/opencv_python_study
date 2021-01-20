faac6c59553a05470e43edf40b5c8490346cd4f6
nhs04047 <nhs04047@naver.com>
2021-01-20 22:29:02 +0900

    update [findContours]

--- P06/findContours.py
+++ P06/findContours.py
@@ -1,5 +1,6 @@
 import sys
 import cv2
+import numpy as np
 
 callingCard = cv2.imread('image1.jpg')
 
@@ -8,7 +9,7 @@ if callingCard is None:
     sys.exit()
 
 callingCard_gray = cv2.cvtColor(callingCard,cv2.COLOR_BGR2GRAY)
-ret, callingCard_bin = cv2.threshold(callingCard_gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+ret, callingCard_bin = cv2.threshold(callingCard_gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
 #cv2.imshow('callingCard', callingCard)
 #cv2.imshow('callingCard_gray', callingCard_gray)
@@ -17,18 +18,28 @@ cv2.imshow('callingCard_bin', callingCard_bin)
 contours, _ = cv2.findContours(callingCard_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 print(contours)
 
-#외각선 근사화
 for pts in contours:
     if cv2.contourArea(pts) < 1000:
         continue
 
+    # 외각선 근사화
     approx = cv2.approxPolyDP(pts, cv2.arcLength(pts, True)*0.02,True)
 
-    if len(approx) != 4:
+    if len(approx) != 4: #사각형 판
         continue
+
+    # 기하학적 변환 - Perspective Transform
+    w, h = 250, 400
+    callingCardQuad = np.array([[approx[0, 0, :]], [approx[1, 0, :]],
+                                [approx[2, 0, :]], [approx[3, 0, :]]]).astype(np.float32)
+    dftQuad = np.array([[0, h], [w, h],[w, 0],[0, 0]]).astype(np.float32)
+
+    pers = cv2.getPerspectiveTransform(callingCardQuad,dftQuad)
+    dst = cv2.warpPerspective(callingCard, pers, (w,h))
 
     cv2.polylines(callingCard, pts, True, (0,0,255))
 
-
+cv2.imshow('callingCard', callingCard)
+cv2.imshow('perspective img', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
