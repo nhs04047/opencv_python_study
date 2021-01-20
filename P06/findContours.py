6991c283e74e069d9b9b7ca9ae11898c479bae31
nhs04047 <nhs04047@naver.com>
2021-01-20 16:53:05 +0900

    Add study files

--- P06/findContours.py
+++ P06/findContours.py
@@ -10,9 +10,25 @@ if callingCard is None:
 callingCard_gray = cv2.cvtColor(callingCard,cv2.COLOR_BGR2GRAY)
 ret, callingCard_bin = cv2.threshold(callingCard_gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-cv2.imshow('callingCard', callingCard)
-cv2.imshow('callingCard_gray', callingCard_gray)
+#cv2.imshow('callingCard', callingCard)
+#cv2.imshow('callingCard_gray', callingCard_gray)
 cv2.imshow('callingCard_bin', callingCard_bin)
+
+contours, _ = cv2.findContours(callingCard_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+print(contours)
+
+#외각선 근사화
+for pts in contours:
+    if cv2.contourArea(pts) < 1000:
+        continue
+
+    approx = cv2.approxPolyDP(pts, cv2.arcLength(pts, True)*0.02,True)
+
+    if len(approx) != 4:
+        continue
+
+    cv2.polylines(callingCard, pts, True, (0,0,255))
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
