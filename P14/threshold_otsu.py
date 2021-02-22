b582e7ba8a3091e2372cf2f2efcb5918bcf01df3
nhs04047 <nhs04047@naver.com>
2021-02-22 23:38:09 +0900

    Add sudy file [threshold_otsu]

--- P14/threshold_otsu.py
+++ P14/threshold_otsu.py
@@ -7,7 +7,11 @@ if src is None:
     print('Image load failed')
     sys.exit()
 
+# or 연산자로 OTSU 인자 입력
+# 반환값 2개, 1개는 OTSU 임계값(실수형), 1개는 dst영상
+# cv2.THRESH_OTSU 만 입력해도 됌
 th, dst = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+print("otsu's threshold:", th)
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
